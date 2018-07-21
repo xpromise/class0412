d5518e0fc77879816f2f6b2c806c711f46370dd5
xiongjian <webjsforyou@gmail.com>
2018-07-21 15:12:46 +0800

    路由

--- day03/02.express中的服务器.js
+++ day03/02.express中的服务器.js
@@ -35,7 +35,7 @@ app.get('/', (request, response) => {
   const query = request.query;
   console.log(query); //{ usename: 'sunwukong', password: '123123' }
   //返回响应
-  // response.send('<h1>这是express服务器返回的响应</h1>');
+  response.send('<h1>这是express服务器返回的响应</h1>');
 })
 
 app.post('/', (request, response) => {
@@ -50,7 +50,7 @@ app.post('/', (request, response) => {
   // const query = request.query;
   // console.log(query); //{ usename: 'sunwukong', password: '123123' }
   //返回响应
-  // response.send('<h1>这是express服务器返回的响应</h1>');
+  response.send('<h1>这是express服务器返回的响应</h1>');
 })
 
 //监听端口号，开启服务
