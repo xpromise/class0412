87bea8b45dfe2757607b8976e1d4672c76ce8807
xiongjian <webjsforyou@gmail.com>
2018-07-24 16:39:36 +0800

    jQuery中ajax的练习完成

--- day05/02.ajax.js
+++ day05/02.ajax.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.static('public'));
 
 app.get('/ajax', (req, res) => {
   console.log(req.query);
+  res.send('这是服务器返回的ajax的响应');
+})
+
+app.post('/ajax', (req, res) => {
+  console.log(req.body);
   res.send('这是服务器返回的ajax的响应');
 })
 
