83d07953ef8e093ddbb0be1d288b836645bd100f
xiongjian <webjsforyou@gmail.com>
2018-08-01 11:09:56 +0800

    js和less文件的处理

--- 02_projectBuild/grunt_test/Gruntfile.js
+++ 02_projectBuild/grunt_test/Gruntfile.js
@@ -17,7 +17,8 @@ module.exports = function (grunt) {
         globals: {
           jQuery: true,
           module: true,
-          console: true
+          console: true,
+          require: true
         },
       },
       all: ['Gruntfile.js', 'src/js/*.js']    //要进行语法检查的文件
@@ -35,11 +36,60 @@ module.exports = function (grunt) {
           dest: 'build/'  //语法转换后输出到此目录下
         }]
       }
+    },
+    concat: {
+      options: {
+        separator: ';',  //分割符
+      },
+      dist: {
+        src: ['build/js/*.js'],  //要合并的源文件
+        dest: 'build/js/built.js',   //合并后输出的文件
+      },
+    },
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+        '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        files: {
+          'dist/js/dist.min.js': ['build/js/built.js']  // 压缩后输出的文件  / 要压缩的源文件
+        }
+      }
+    },
+    less: {
+      production: {
+        options: {
+          paths: ['build/css'],
+          plugins: [
+            new (require('less-plugin-autoprefix'))({browsers: ["last 2 versions", "not ie <= 8"]}),  //扩展前缀
+          ]
+        },
+        files: {
+          'build/css/index.css': 'src/less/*.less'  //   编译后的css文件  / 要编译的less文件
+        }
+      }
+    },
+    cssmin: {
+      options: {
+        mergeIntoShorthands: false,   //不要合并css
+        roundingPrecision: -1         //不要四舍五入
+      },
+      target: {
+        files: {
+          'dist/css/index.min.css': ['build/css/index.css']
+        }
+      }
     }
   });
   //2. 加载任务插件
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   //3. 注册默认任务
-  grunt.registerTask('default', ['jshint', 'babel']);  //grunt执行任务是同步的，从左到右依次执行
+  grunt.registerTask('default', ['jshint', 'babel', 'concat', 'uglify', 'less', 'cssmin']);  //grunt执行任务是同步的，从左到右依次执行
 };
